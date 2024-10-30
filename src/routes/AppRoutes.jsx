96ec6d2f1e884c7d0f02e000b720e42534e8771c
hoangtb24 <100108328+HoangACM@users.noreply.github.com>
2024-10-30 21:04:27 +0700

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "../components/Login";
 import Home from "../components/Home";
 import ListUsers from "../components/ListUsers"
